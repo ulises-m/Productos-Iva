c853ae5987ac74e0726ebc3773c895af58a3c15d
ulises-m <ulises_cruz_19@outlook.com>
2014-11-30 22:15:48 -0600

    Initial commit

---
+++ Formulario.php
@@ -0,0 +1,44 @@
+<html>
+     <head>
+         <title>Practica#3</title>
+     </head>
+ <body>
+  <form method="POST" action="Guardar.php">
+
+    <b>Practica#3</b>
+    <br>
+    <b>Productos con IVA</b>
+    </br>
+   
+      <table border="1">
+      <tr>
+        <td><strong>Cantidad</strong></td>
+        <td><strong>Poducto</strong></td>
+        <td><strong>Precio</strong></td>
+      </tr>
+       
+      <tr>
+        <td><input type="text" size="25" value="" name="cantidad1"></td>
+        <td><input type="text" size="25" value="" name="producto1"></td>
+        <td><input type="text" size="25" value="" name="precio1"></td>
+      </tr>
+       
+      <tr>
+        <td><input type="text" size="25" value="" name="cantidad2"></td>
+        <td><input type="text" size="25" value="" name="producto2"></td>
+        <td><input type="text" size="25" value="" name="precio2"></td>
+      </tr>
+       
+      <tr>
+        <td><input type="text" size="25" value="" name="cantidad3"></td>
+        <td><input type="text" size="25" value="" name="producto3"></td>
+        <td><input type="text" size="25" value="" name="precio3"></td>
+      </tr>
+      </table>
+
+
+
+   <input type="submit" value="Enviar" ><br>
+  </form>
+ </body>
+</html>
